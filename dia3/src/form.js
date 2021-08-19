617d94726ef7b93fc0c47d6a730452db7ab2ee97
Leticia Fontoura <leticia.fontoura@live.com>
2021-08-19 15:51:15 -0300

    capitalize first letter but something wrong with da dos de etc

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -2,32 +2,32 @@ const input = document.querySelector("[data-js='name']");
 
 input.addEventListener("input", (e) => {
 
-  // console.log("e.target.value", e.target.value);
-  // e.target.value = "a".repeat(e.target.value.length);
-  const nameChange = e.target.value;
-  // if (nameChange.includes("DA ")) {
-  //   e.target.value = nameChange.replace("DA ", "da ");
-  // } else if (nameChange.includes("dA ")) {
-  //   e.target.value = nameChange.replace("dA ", "da ");
+  let nameChange = e.target.value;
+  const nameChangeArr = nameChange.split(" ");
+
+  // for (let i = 0; i < nameChangeArr; i++) {
+  //   newArrNames.push(nameChangeArr[i].charAt(0).toUpperCase() + nameChangeArr[i].slice(1));
   // }
 
-  // nameChange.includes("DA ") ? e.target.value = nameChange.replace("DA ", "da ") : nameChange.includes("Da ") ? e.target.value = nameChange.replace("Da ", "da ") : nameChange.includes("dA ") ? e.target.value = nameChange.replace("dA ", "da ") : ""
+const newArrNames = [];
 
-  // const regex = /da|das|do|dos|de/gi;
-  // console.log(nameChange.replace(regex, "da"))
+  nameChangeArr.forEach(name => {
+   newArrNames.push(name.charAt(0).toUpperCase() + name.slice(1));
+  })
+
+  console.log(newArrNames)
+
+  nameChange = newArrNames.join(" ");
+  e.target.value = nameChange;
+
   if (nameChange.includes(" DA ") || nameChange.includes(" Da ") || nameChange.includes(" dA ")) {
     e.target.value = nameChange.replace(/ da /gi, " da ")
   } else if (nameChange.includes(" DO ") || nameChange.includes(" Do ") || nameChange.includes(" dO ")) {
     e.target.value = nameChange.replace(/ do /gi, " do ")
   } else if (nameChange.includes(" DE ") || nameChange.includes(" De ") || nameChange.includes(" dE ")) {
     e.target.value = nameChange.replace(/ de /gi, " de ")
-  } else if (nameChange.includes(" DOS ") || nameChange.includes(" Dos ") || nameChange.includes(" dOs ") || nameChange.includes(" dOS ") || nameChange.includes(" doS ")) {
+  } else if (nameChange.includes(" DOS ") || nameChange.includes(" Dos ") || nameChange.includes(" dOs ") || nameChange.includes(" dOS ") || nameChange.includes(" doS ") || nameChange.includes(" DoS ") ) {
     e.target.value = nameChange.replace(/ dos /gi, " dos ")
   }
 
-
-  // e.target.value = nameChange.replace(/da /gi, "da ")
-  // e.target.value = nameChange.replace(/da/gi, "da")
-  // e.target.value = nameChange.replace(/da/gi, "da")
-  // e.target.value = nameChange.replace(/da/gi, "da")
 })
