f91b050207b2828f340caa574543766d5f92d016
Leticia Fontoura <leticia.fontoura@live.com>
2021-08-20 11:06:40 -0300

    100% finished with exercise 3 improving the rendering of items inside the table

--- dia3/src/cars.js
+++ dia3/src/cars.js
@@ -6,40 +6,39 @@ carsForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const carImageInput = document.querySelector("[data-js='car-image']");
-  const carRow = document.createElement("tr");
-  carTableInformation.appendChild(carRow);
   const carImageValue = carImageInput.value;
-  const carImageCell = document.createElement("td");
-  carImageCell.innerHTML = carImageValue;
-  carRow.appendChild(carImageCell);
 
   const carMakeModelInput = document.querySelector("[data-js='car-make-model']");
-  carTableInformation.appendChild(carRow);
   const carMakeModelValue = carMakeModelInput.value;
-  const carMakeModelCell = document.createElement("td");
-  carMakeModelCell.innerHTML = carMakeModelValue;
-  carRow.appendChild(carMakeModelCell);
 
   const carYearInput = document.querySelector("[data-js='car-year']");
-  carTableInformation.appendChild(carRow);
   const carYearValue = carYearInput.value;
-  const carYearCell = document.createElement("td");
-  carYearCell.innerHTML = carYearValue;
-  carRow.appendChild(carYearCell);
 
   const carLicensePlateInput = document.querySelector("[data-js='car-license-plate']");
-  carTableInformation.appendChild(carRow);
   const carLicensePlateValue = carLicensePlateInput.value;
-  const carLicensePlateCell = document.createElement("td");
-  carLicensePlateCell.innerHTML = carLicensePlateValue;
-  carRow.appendChild(carLicensePlateCell);
 
   const carColorInput = document.querySelector("[data-js='car-color']");
-  carTableInformation.appendChild(carRow);
   const carColorValue = carColorInput.value;
-  const carColorCell = document.createElement("td");
-  carColorCell.innerHTML = carColorValue;
-  carRow.appendChild(carColorCell);
+
+  const itemsObj = {
+    carImage: carImageValue,
+    carMakeModel: carMakeModelValue,
+    carYear: carYearValue,
+    carLicensePlate: carLicensePlateValue,
+    carColor: carColorValue
+  }
+
+  function renderTable(items) {
+    carTableInformation.insertAdjacentHTML("beforebegin", `<tr>
+    <td>${items.carImage}</td>
+    <td>${items.carMakeModel}</td>
+    <td>${items.carYear}</td>
+    <td>${items.carLicensePlate}</td>
+    <td>${items.carColor}</td>
+  </tr>`)
+  }
+
+  renderTable(itemsObj);
 
   carImageInput.value = "";
   carMakeModelInput.value = "";
