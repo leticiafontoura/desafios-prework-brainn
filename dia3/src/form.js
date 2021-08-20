b76f8976b8533217f29bb9710a309883404b8ee9
Leticia Fontoura <leticia.fontoura@live.com>
2021-08-19 23:10:36 -0300

    strill trying to improve exercise 2 to behave properly

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -107,37 +107,57 @@ const colorsSelect = document.querySelector("[data-js='select-colors']");
 const colorsDiv = document.createElement("div");
 colorsDiv.style.display = "flex";
 form.appendChild(colorsDiv);
-colorsDiv.innerHTML = `<div class="blue" data-js="blue"></div><div class="green" data-js="green"></div><div class="red" data-js="red"></div><div class="purple" data-js="purple"></div><div class="black" data-js="black"></div>`
+// colorsDiv.innerHTML = `<div class="blue" data-js="blue"></div><div class="green" data-js="green"></div><div class="red" data-js="red"></div><div class="purple" data-js="purple"></div><div class="black" data-js="black"></div>`
+
+const blue = document.createElement("div");
+blue.setAttribute("class", "blue");
+colorsDiv.appendChild(blue);
+
+const green = document.createElement("div");
+green.setAttribute("class", "green");
+colorsDiv.appendChild(green);
+
+const red = document.createElement("div");
+red.setAttribute("class", "red");
+colorsDiv.appendChild(red);
+
+const purple = document.createElement("div");
+purple.setAttribute("class", "purple");
+colorsDiv.appendChild(purple);
+
+const black = document.createElement("div");
+black.setAttribute("class", "black");
+colorsDiv.appendChild(black);
+
 
 colorsSelect.addEventListener("click", (e) => {
 
   if (e.target.value === "Blue") {
-    const blue = document.querySelector("[data-js='blue']");
-    console.log(blue)
+    // const blue = document.querySelector("[data-js='blue']");
     blue.classList.toggle("with-color");
   }
 
   if (e.target.value === "Green") {
-    const green = document.querySelector("[data-js='green']");
-    console.log(green)
+    // const green = document.querySelector("[data-js='green']");
+    // console.log(green)
     green.classList.toggle("with-color");
   }
 
   if (e.target.value === "Red") {
-    const red = document.querySelector("[data-js='red']");
-    console.log(red)
+    // const red = document.querySelector("[data-js='red']");
+    // console.log(red)
     red.classList.toggle("with-color");
   }
 
   if (e.target.value === "Purple") {
-    const purple = document.querySelector("[data-js='purple']");
-    console.log(purple)
+    // const purple = document.querySelector("[data-js='purple']");
+    // console.log(purple)
     purple.classList.toggle("with-color");
   }
 
   if (e.target.value === "Black") {
-    const black = document.querySelector("[data-js='black']");
-    console.log(black)
+    // const black = document.querySelector("[data-js='black']");
+    // console.log(black)
     black.classList.toggle("with-color");
   }
 })
