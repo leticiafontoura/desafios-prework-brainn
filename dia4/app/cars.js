b542510a90de1c67b5ef595ed6af6c53f2ab74d7
Leticia Fontoura <leticia.fontoura@live.com>
2021-08-21 15:01:25 -0300

    exercise 3 full completed

--- dia4/app/cars.js
+++ dia4/app/cars.js
@@ -5,31 +5,31 @@ function refreshTable() {
   console.log("Refreshing table..");
 
   fetch("http://localhost:3333/cars")
-  .then((response) => response.json())
-  .then((cars) => {
-    if (cars.length === 0) {
-      table.insertAdjacentHTML("beforeend", `<tbody><tr><td colspan="5">Nenhum carro cadastrado</td></tr></tbody>`)
-      return;
-    }
+    .then((response) => response.json())
+    .then((cars) => {
+      if (cars.length === 0) {
+        table.insertAdjacentHTML("beforeend", `<tbody><tr><td colspan="5">Nenhum carro cadastrado</td></tr></tbody>`)
+        return;
+      }
 
-    // If tbody already exists, we simply remove it, because we'll always create a new one.
-    const existingTbody = document.querySelector("tbody");
-    if (existingTbody) {
-      existingTbody.remove();
-    }
+      // If tbody already exists, we simply remove it, because we'll always create a new one.
+      const existingTbody = document.querySelector("tbody");
+      if (existingTbody) {
+        existingTbody.remove();
+      }
 
-    const tbody = document.createElement("tbody");
-    table.appendChild(tbody);
-    for (let i = 0; i < cars.length; i++) {
-      tbody.insertAdjacentHTML("beforeend", `<tr data-js='table-row'>
+      const tbody = document.createElement("tbody");
+      table.appendChild(tbody);
+      for (let i = 0; i < cars.length; i++) {
+        tbody.insertAdjacentHTML("beforeend", `<tr data-js='table-row'>
         <td>${cars[i].image}</td>
         <td>${cars[i].brandModel}</td>
         <td>${cars[i].year}</td>
         <td>${cars[i].plate}</td>
         <td>${cars[i].color}</td>
         </tr>`)
-    }
-  })
+      }
+    })
 }
 
 refreshTable();
@@ -53,20 +53,36 @@ carsForm.addEventListener("submit", (e) => {
   const carColorValue = carColorInput.value;
 
   fetch("http://localhost:3333/cars", {
-    method: "POST",
-    body: JSON.stringify({
-      image: carImageValue,
-      brandModel: carMakeModelValue,
-      year: carYearValue,
-      plate: carLicensePlateValue,
-      color: carColorValue
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  }).then(() => {
-    refreshTable();
-  })
+      method: "POST",
+      body: JSON.stringify({
+        image: carImageValue,
+        brandModel: carMakeModelValue,
+        year: carYearValue,
+        plate: carLicensePlateValue,
+        color: carColorValue
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    }).then((response) => response.json())
+    .then((response) => {
+      const errorMessage = document.querySelector("[data-js='error-message']");
+      errorMessage.innerHTML = response.message;
+      if (response.error) {
+        errorMessage.style.display = "block";
+      } else {
+        errorMessage.style.display = "none"
+        refreshTable();
+      }
+    })
+  // {
+  //   console.log(response.json())
+  //   // if (response.error === true) {
+  //   //   console.log("errouuuu")
+  //   // }
+
+  //   refreshTable();
+  // })
 
   carImageInput.value = "";
   carMakeModelInput.value = "";
